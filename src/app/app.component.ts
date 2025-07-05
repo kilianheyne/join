1c56ee76ee3874ab234231c5d4c83fdbcb91ad2c
iri-ko <irene.kober@gmx.net>
2025-07-05 04:36:04 +0200

    Added contactlist basic strucure and design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactpageComponent } from "./contactpage/contactpage.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
