47c63cf035b1bc863a029ec31e32c1bfe588dbbf
Kilian <kilianheyne@icloud.com>
2025-07-07 10:03:27 +0200

    reworked layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
-import { SidebarComponent } from "./shared/header/sidebar/sidebar.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { ContactpageComponent } from "./contactpage/contactpage.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, ContactpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from "./sidebar/sidebar.component";
 
 @Component({
   selector: 'app-header',
-  imports: [SidebarComponent],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
