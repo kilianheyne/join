8a97191283cf3df0a26e5db2469efae91ba7664b
Kilian <kilianheyne@icloud.com>
2025-07-08 12:48:03 +0200

    created a mobile contact-details-components with first iteration of HTML

---
+++ src/app/contactpage/contact-details-mobile/contact-details-mobile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-details-mobile',
+  imports: [],
+  templateUrl: './contact-details-mobile.component.html',
+  styleUrl: './contact-details-mobile.component.scss'
+})
+export class ContactDetailsMobileComponent {
+
+}
