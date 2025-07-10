5c1181911434ed0da91e15d2ab156485eecc78d8
Kilian <kilianheyne@icloud.com>
2025-07-10 13:26:50 +0200

    addded finishing touches to TypeScript as well as description

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   //#region attributes
   showMenu: boolean = false; //toggleFlag for (click) at user -> opens user-menu
   isMobile: boolean = false; //toggleFlag, adds "Help" to user-menu
@@ -15,10 +15,21 @@ export class HeaderComponent {
   //#endregion
 
   //#region methods
-  toggleMenu(): void {
+  toggleMenu(): void { //simple toggle-function to change the state of showMenu
     this.showMenu = !this.showMenu;
   }
 
+  ngOnInit(): void { //checks, if the user-menu should show "help" or not "onload"
+    this.checkIfMobile();
+  }
+
+  checkIfMobile(): void { //changes state of isMobile depending on the current screen-size -> limit is 820px in width,, smaller than 820px shows help in user-menu
+    this.isMobile = window.innerWidth <= 820;
+  }
+
+  @HostListener('window:resize') onResize() { //changes state of isMobile dynamically 
+    this.checkIfMobile();
+  }
 
   //#endregion
 }
