90fb4abdb48c27017196be2fe414ebe6965f391e
Kilian <kilianheyne@icloud.com>
2025-07-10 13:14:53 +0200

    added a base structure for a user menu by clicking on user-profile

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  //#region attributes
+  showMenu: boolean = false; //toggleFlag for (click) at user -> opens user-menu
+  isMobile: boolean = false; //toggleFlag, adds "Help" to user-menu
 
+
+  //#endregion
+
+  //#region methods
+  toggleMenu(): void {
+    this.showMenu = !this.showMenu;
+  }
+
+
+  //#endregion
 }
