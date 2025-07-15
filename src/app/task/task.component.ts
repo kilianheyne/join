74fc88a12f73d6e68c5994407e36990f18a14ae0
Jan Gerardi <jan_gerardi@outlook.com>
2025-07-15 20:59:37 +0200

    feature/task-overlay

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task',
+  imports: [CommonModule],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.scss'
+})
+export class TaskComponent {
+
+}
