cb25a6b1955897295cd13e9789f2e3bfd882c23f
Kilian <kilianheyne@icloud.com>
2025-07-15 11:06:32 +0200

    added styling to board-header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ContactpageComponent } from './contactpage/contactpage.component';
 import { BoardComponent } from './board/board.component';
 
 export const routes: Routes = [
+    { path: '', component: BoardComponent },
     { path: 'board', component: BoardComponent },
     { path: 'contacts', component: ContactpageComponent },
 ];
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { BlackButtonComponent } from "../general/black-button/black-button.component";
+import { InputComponent } from "../general/input/input.component";
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [BlackButtonComponent, InputComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
