3e9eedb91979446e5450113f7cf907e1433fb9eb
Kilian <kilianheyne@icloud.com>
2025-07-15 13:23:04 +0200

    added finishing touches to desktop-css

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { BlackButtonComponent } from "../general/black-button/black-button.component";
-import { InputComponent } from "../general/input/input.component";
 
 @Component({
   selector: 'app-board',
-  imports: [BlackButtonComponent, InputComponent],
+  imports: [BlackButtonComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
