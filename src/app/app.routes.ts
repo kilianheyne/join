7b28a4d04c9023f0cd5bf59c4c444d240fa71ce7
Samaneh Papoli <faalsamaneh@gmail.com>
2025-07-15 18:12:30 +0200

    add Add Task route to app route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { ContactpageComponent } from './contactpage/contactpage.component';
+import { AddTaskPageComponent } from './add-task-page/add-task-page.component';
 
 export const routes: Routes = [
+    {
+        path: 'add-task',
+        component: AddTaskPageComponent
+    },
     {
         path: 'contacts',
         component: ContactpageComponent
