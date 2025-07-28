1d88ff451e470890e111640574e73689852369a0
iri-ko <irene.kober@gmx.net>
2025-07-28 15:46:56 +0200

    added long press for open task details

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -123,6 +123,32 @@ export class BoardComponent {
     this.tasks = [...this.tasks];
   }
 
+  private longPressTimer: any;
+  private isLongPress = false;
+
+  handleMouseDown(task: Task): void {
+    if (this.isMobile) {
+      this.longPressTimer = setTimeout(() => {
+        this.isLongPress = true;
+        this.openTaskDetails(task);
+      }, 300);
+    }
+  }
+
+  handleMouseUp(): void {
+    clearTimeout(this.longPressTimer);
+  }
+
+  handleClick(task: Task): void {
+    if (!this.isMobile) {
+      this.openTaskDetails(task);
+    } else if (!this.isLongPress) {
+      // Ignore short tap
+    }
+    this.isLongPress = false;
+  }
+
+
   openTaskDetails(task: Task) {
     this.selectedTask = task;
     this.isTaskDetailsVisible = true;
@@ -160,7 +186,7 @@ export class BoardComponent {
   }
 
   openMoveTaskMenu(task: Task): void {
-    const ref = this.bottomSheet.open(MoveTaskSheetComponent, { data: { task }});
+    const ref = this.bottomSheet.open(MoveTaskSheetComponent, { data: { task } });
 
     ref.afterDismissed().subscribe((newStatus: TaskStatus) => {
       if (newStatus && task.id) {
