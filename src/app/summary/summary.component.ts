3c510824ed1768e14d90209da567591ca5fdd059
Kilian <kilianheyne@icloud.com>
2025-07-28 19:44:40 +0200

    added dynamic rendering to numbers of summary

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { Task } from '../interfaces/task';
+import { Timestamp } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-summary',
@@ -8,4 +11,52 @@ import { Component } from '@angular/core';
 })
 export class SummaryComponent {
 
+  toDoCount: number = 0;
+  doneCount: number = 0;
+  urgentCount: number = 0;
+  taskCount: number = 0;
+  inProgressCount: number = 0;
+  awaitingFeedbackCount: number = 0;
+  nextDeadline: string | null = null; 
+
+  constructor(private firebaseService: FirebaseService) {}
+
+  ngOnInit(): void {
+    this.loadTaskSummary();
+  }
+
+  loadTaskSummary() {
+    this.firebaseService.tasksList$.subscribe((tasks: Task[]) => {
+      this.toDoCount = tasks.filter(task => task.status === 'to-do').length;
+      this.doneCount = tasks.filter(task => task.status === 'done').length;
+      this.urgentCount = tasks.filter(task => task.priority === 'Cswm909cWSJhKcu5vB14').length;
+      this.taskCount = tasks.length;
+      this.inProgressCount = tasks.filter(task => task.status === 'in-progress').length;
+      this.awaitingFeedbackCount = tasks.filter(task => task.status === 'await-feedback').length;
+
+      const urgentTasks = tasks
+        .filter(task => task.priority === 'Cswm909cWSJhKcu5vB14' && task.date && typeof (task.date as any).toDate === 'function')
+        .map(task => ({
+          ...task,
+          date: (task.date as any).toDate()
+        }))
+        .sort((a, b) => a.date.getTime() - b.date.getTime());
+
+        if (urgentTasks.length > 0) {
+          const nextDate = urgentTasks[0].date;
+          this.nextDeadline = nextDate.toLocaleDateString('de-DE', {
+            weekday: 'long',
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+          });
+        } else {
+          this.nextDeadline = null;
+        }
+    });
+  }
+
+  // loadNextDeadline() {
+
+  // }
 }
