d99fcc722799f21c0e25c23e1501eff6c5f7b695
iri-ko <irene.kober@gmx.net>
2025-07-30 15:45:05 +0200

    added basic oultine for sign-up-form

--- src/app/login-screen/login-screen.component.ts
+++ src/app/login-screen/login-screen.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AddTaskComponent } from "../board/add-task/add-task.component";
 import { BlackButtonComponent } from "../general/black-button/black-button.component";
 import { LoginFormComponent } from './login-form/login-form.component';
+import { SignUpFormComponent } from "./sign-up-form/sign-up-form.component";
 
 @Component({
   selector: 'app-login-screen',
-  imports: [AddTaskComponent, BlackButtonComponent, LoginFormComponent,],
+  imports: [AddTaskComponent, BlackButtonComponent, LoginFormComponent, SignUpFormComponent],
   templateUrl: './login-screen.component.html',
   styleUrl: './login-screen.component.scss'
 })
---
+++ src/app/login-screen/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BlackButtonComponent } from "../../general/black-button/black-button.component";
+
+@Component({
+  selector: 'app-sign-up-form',
+  imports: [BlackButtonComponent],
+  templateUrl: './sign-up-form.component.html',
+  styleUrl: './sign-up-form.component.scss'
+})
+export class SignUpFormComponent {
+
+}
