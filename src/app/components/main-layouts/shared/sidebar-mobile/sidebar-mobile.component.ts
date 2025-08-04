0cce96980d8bbb820fd6b91d9b9a68b39c6ae404
Jan Gerardi <jan_gerardi@outlook.com>
2025-08-04 09:42:49 +0200

    feature/added restricted sidebar

--- src/app/components/main-layouts/shared/sidebar-mobile/sidebar-mobile.component.ts
+++ src/app/components/main-layouts/shared/sidebar-mobile/sidebar-mobile.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { CdkDragPlaceholder } from "@angular/cdk/drag-drop";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar-mobile',
-  imports: [RouterLink, RouterModule],
+  imports: [RouterLink, RouterModule, CdkDragPlaceholder, CommonModule],
   templateUrl: './sidebar-mobile.component.html',
   styleUrl: './sidebar-mobile.component.scss'
 })
 export class SidebarMobileComponent {
 
+  isLoggedIn: boolean = false;
 }
--- src/app/components/main-layouts/shared/sidebar/sidebar.component.ts
+++ src/app/components/main-layouts/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, RouterModule],
+  imports: [RouterLink, RouterModule, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
 
+  isLoggedIn: boolean = false;
 }
